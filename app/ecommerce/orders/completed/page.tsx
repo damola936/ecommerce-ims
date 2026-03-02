1bb19165ddbdf51afe17680e525410dc5275f335
Recompense <damolajimoh100@gmail.com>
2026-03-02 10:52:09 +0100

    feat: Add  order and customer management, data tables, and essential UI components.

--- app/ecommerce/orders/completed/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CompletedOrdersPage() {
-    return (
-        <div></div>
-    );
-}
-
-export default CompletedOrdersPage;
--- app/ecommerce/orders/pending/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function PendingOrdersPage() {
-    return (
-        <div></div>
-    );
-}
-
-export default PendingOrdersPage;
